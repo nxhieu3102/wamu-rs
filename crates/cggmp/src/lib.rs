8daf7a4ad4569b03b4156664f91ca74c08f026ac
davidsemakula <hello@davidsemakula.com>
2023-07-30 22:19:46 +0300

    add identity authentication and quorum approval state machines

--- crates/cggmp/src/lib.rs
+++ crates/cggmp/src/lib.rs
@@ -1,14 +1,17 @@
 //! A Rust implementation of [CGGMP20](https://eprint.iacr.org/2021/060.pdf) with augmentations as described by the [Wamu protocol](https://wamu.tech/specification) for building threshold signature wallets controlled by multiple decentralized identities.
 
 pub use self::{
-    errors::Error, identity_rotation::IdentityRotation, key_refresh::AugmentedKeyRefresh,
-    keygen::AugmentedKeyGen, sign::AugmentedPreSigning, sign::AugmentedSigning,
+    errors::Error, identity_auth::IdentityAuthentication, identity_rotation::IdentityRotation,
+    key_refresh::AugmentedKeyRefresh, keygen::AugmentedKeyGen, quorum_approval::QuorumApproval,
+    sign::AugmentedPreSigning, sign::AugmentedSigning,
 };
 
 #[macro_use]
 pub mod asm;
 mod errors;
+mod identity_auth;
 mod identity_rotation;
 mod key_refresh;
 mod keygen;
+mod quorum_approval;
 mod sign;
