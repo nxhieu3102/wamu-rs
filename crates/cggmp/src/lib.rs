31d0081aafbbb1b342889d9ff661856eb299f6a5
davidsemakula <hello@davidsemakula.com>
2023-07-30 11:18:21 +0300

    add identity rotation state machine

--- crates/cggmp/src/lib.rs
+++ crates/cggmp/src/lib.rs
@@ -1,13 +1,14 @@
 //! A Rust implementation of [CGGMP20](https://eprint.iacr.org/2021/060.pdf) with augmentations as described by the [Wamu protocol](https://wamu.tech/specification) for building threshold signature wallets controlled by multiple decentralized identities.
 
 pub use self::{
-    errors::Error, key_refresh::AugmentedKeyRefresh, keygen::AugmentedKeyGen,
-    sign::AugmentedPreSigning, sign::AugmentedSigning,
+    errors::Error, identity_rotation::IdentityRotation, key_refresh::AugmentedKeyRefresh,
+    keygen::AugmentedKeyGen, sign::AugmentedPreSigning, sign::AugmentedSigning,
 };
 
 #[macro_use]
 pub mod asm;
 mod errors;
+mod identity_rotation;
 mod key_refresh;
 mod keygen;
 mod sign;
